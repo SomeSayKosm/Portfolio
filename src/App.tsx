adc682df3210ddc517b0954f588d5bdeef05fe19
Carson Hoff <hoffcarson@gmail.com>
2025-11-16 14:34:55 -0500

    Move Font Awesome library import to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { useState } from 'react'
 import clsx from 'clsx';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
 import NavBar from './NavBar'
+
+library.add(fas, far, fab)
 
 function App() {
   const [ darkModeEnabled, setDarkModeEnabled ] = useState(false);
--- src/DarkModeToggle.tsx
+++ src/DarkModeToggle.tsx
@@ -1,11 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
 import clsx from "clsx"
-
-library.add(fas, far, fab)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 // @ts-ignore
 const moonIcon : IconProp = "fa-solid fa-moon";
