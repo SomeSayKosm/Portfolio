3d2241b2ec35f848abbfc3513eee30e60cf75f88
Carson Hoff <hoffcarson@gmail.com>
2025-11-22 15:10:28 -0500

    Update NavBarDropdown to handle DarkMode

--- src/NavBarDropdown.tsx
+++ src/NavBarDropdown.tsx
@@ -17,7 +17,8 @@ type Props = {
 const NavBarDropdown = ({ darkModeEnabled, optionalStyle, dropdownOptions, children }: Props) => {
   const [ isHovered, setIsHovered ] = useState(false);
 
-  const colorSettings = darkModeEnabled ? "border-blue-600 hover:bg-orange-500 hover:text-slate-800" : "border-amber-600 hover:bg-rose-400 hover:text-slate-800";
+  const baseColorSettings = darkModeEnabled ? "border-blue-600 hover:bg-orange-400 hover:text-slate-800" : "border-amber-600 hover:bg-rose-400 hover:text-slate-800";
+  const listColorSettings = darkModeEnabled ? "border-blue-600 shadow-[-2px_0_0_0_rgba(37,99,235,1)]" : "border-amber-600 shadow-[-2px_0_0_0_rgba(217,119,6,1)]";
   
   const dropdownOptionItems = dropdownOptions.map((option, index) => {
     return <NavBarOption key={index} darkModeEnabled={darkModeEnabled} optionalStyle={"border-t-2"}>
@@ -29,7 +30,7 @@ const NavBarDropdown = ({ darkModeEnabled, optionalStyle, dropdownOptions, child
     <li 
       className={clsx(
         "flex flex-col px-3 py-1 cursor-pointer list-none font-semibold relative box-content",
-        colorSettings, 
+        baseColorSettings, 
         optionalStyle
       )}
       onMouseEnter={() => setIsHovered(true)}
@@ -43,7 +44,10 @@ const NavBarDropdown = ({ darkModeEnabled, optionalStyle, dropdownOptions, child
             />      
       </div>
       {isHovered &&
-        <ul className="absolute border-r-2 border-b-2 shadow-[-2px_0_0_0_rgba(217,119,6,1)] border-amber-600 top-8 left-0 w-full bg-amber-400 box-content">
+        <ul className={clsx(
+          "absolute border-r-2 border-b-2 top-8 left-0 w-full bg-amber-400 box-content",
+          listColorSettings
+        )}>
           {dropdownOptionItems}
         </ul>
       }
--- src/NavBarOption.tsx
+++ src/NavBarOption.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const NavBarOption = ({ darkModeEnabled, optionalStyle, children }: Props) => {
-    const colorSettings = darkModeEnabled ? "border-blue-600 hover:bg-orange-500 hover:text-slate-800" : "border-amber-600 hover:bg-rose-400 hover:text-slate-800";
+    const colorSettings = darkModeEnabled ? "border-blue-600 hover:bg-orange-400 hover:text-slate-800" : "border-amber-600 hover:bg-rose-400 hover:text-slate-800";
     
     return <li className={clsx(
       "px-3 py-1 cursor-pointer list-none font-semibold",
