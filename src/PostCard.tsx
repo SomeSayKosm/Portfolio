dcd42451a47773521e81dfc6b75d0be007de0d70
Carson Hoff <hoffcarson@gmail.com>
2025-11-28 22:49:47 -0500

    Set DarkMode stylings for PostCard

--- src/PostCard.tsx
+++ src/PostCard.tsx
@@ -1,7 +1,6 @@
 import clsx from "clsx";
 
 type Props = {
-    darkModeEnabled: boolean,
     data: {
         title: string,
         content: string,
@@ -9,33 +8,28 @@ type Props = {
     }
 }
 
-const PostCard = ({darkModeEnabled, data}: Props) => {
-    const colorSettings = darkModeEnabled ? 
-    { 
-        body: "bg-blue-900",
-        primaryText: "text-slate-200 border-blue-600",
-        secondaryText: "",
-    } : {
-        body: "bg-amber-200",
-        primaryText: "text-orange-950 border-amber-600",
-        secondaryText: "text-orange-800"
-    };
+const PostCard = ({data}: Props) => {
+    const colorSettings = { 
+        body: "dark:bg-blue-900 bg-amber-200",
+        primaryText: "dark:text-slate-200 dark:border-blue-600 text-orange-950 border-amber-600",
+        secondaryText: "dark:text-indigo-300 text-orange-800",
+    }  
 
     return (
         <div 
             className={clsx(
-                "m-4 p-1 rounded-xl border-2 border-amber-500 bg-amber-50"
+                "m-4 p-1 rounded-xl border-2 dark:bg-sky-950 dark:border-cyan-600 border-amber-500 bg-amber-50 "
         )}>
             <div 
                 className={clsx(
-                    "p-8 rounded-xl border-2 border-amber-600",
+                    "p-8 rounded-xl border-2 dark:border-cyan-500 border-amber-600",
                     colorSettings.body
             )}>
                 <div 
                     className={clsx(
                         "text-3xl font-semibold",
                         colorSettings.primaryText
-                )}>
+                    )}>
                     {data.title}
                 </div>
                 <div 
